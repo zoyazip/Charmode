1f328489c7325ac1404a3fd3429451f81a292545
nikita <nikita@plotnikov.lv>
2024-07-09 15:31:00 +0300

    minor navbar tree structure change

--- resources/views/web/layout/header.blade.php
+++ resources/views/web/layout/header.blade.php
@@ -1,19 +1,19 @@
-<nav class="navbar">
-    <div class="navbar-top">
-        <img src="{{ URL::asset('assets/svg/logo.svg') }}"></img>
-        <div class="search-container">
+<div class="navbar">
+    <div class="navbar__top">
+        <img src="assets/svg/logo.svg"></img>
+        <div class="navbar__search-container">
             <input type="text" placeholder="Find your cheapest luxury brand...">
         </div>
-        <div class="navbar-icons">
-            <img src="{{ URL::asset('assets/svg/shopping-basket.svg') }}" alt="Cart">
-            <img src="{{ URL::asset('assets/svg/user.svg') }}" alt="User">
+        <div class="navbar__icons">
+            <img src="assets/svg/shopping-basket.svg" alt="Cart">
+            <img src="assets/svg/user.svg" alt="User">
         </div>
     </div>
-    <div class="navbar-links">
-        <div class="link-container active"><a href="/">Home</a></div>
-        <div class="link-container"><a href="about">About</a></div>
-        <div class="link-container"><a href="services">Services</a></div>
-        <div class="link-container"><a href="products">Products</a></div>
-        <div class="link-container"><a href="contact">Contact</a></div>
+    <div class="navbar__links">
+        <div class="navbar__link-container navbar--active"><a href="#home">Homezzz</a></div>
+        <div class="navbar__link-container"><a href="#about">About</a></div>
+        <div class="navbar__link-container"><a href="#services">Services</a></div>
+        <div class="navbar__link-container"><a href="#products">Products</a></div>
+        <div class="navbar__link-container"><a href="#contact">Contact</a></div>
     </div>
-</nav>
+</div>
