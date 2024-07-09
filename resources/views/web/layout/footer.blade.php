ca4880f47fff7c09cc94540867e6b1ac1ec95271
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-07-09 17:46:30 +0300

    Updated footer

--- resources/views/web/layout/footer.blade.php
+++ resources/views/web/layout/footer.blade.php
@@ -1,30 +1,34 @@
-<div class="footerwrapper">
+<div class="inner-container">
+    <div class="footerwrapper">
+        <div class="footerwrapper__left-Footer">
+            <h1>Discover Timeless Elegance</h1>
+            <p>Step into a world where luxury meets craftsmanship. At Charmode, we curate exquisite furniture pieces
+                that
+                transform spaces into sophisticated sanctuaries. Each creation reflects an unwavering commitment to
+                quality
+                and an eye for detail, ensuring your home exudes unparalleled style and comfort.</p>
+        </div>
 
-    <div class="footerwrapper__left-Footer">
-        <h1>Discover Timeless Elegance</h1>
-        <p>Step into a world where luxury meets craftsmanship. At Charmode, we curate exquisite furniture pieces that
-            transform spaces into sophisticated sanctuaries. Each creation reflects an unwavering commitment to quality
-            and an eye for detail, ensuring your home exudes unparalleled style and comfort.</p>
+        <div class="footerwrapper__right-Footer">
+            <h1> Subscribe to Our Luxury Living Newsletter</h1>
+            <form>
+                <input type="text" class="footerwrapper__text-input" name="userName" placeholder="Your name">
+                <input type="email" class="footerwrapper__text-input" placeholder="Your email">
+                <input type="submit" class="footerwrapper__subscribe-btn" value="Subscribe">
+            </form>
+        </div>
+
     </div>
-
-    <div class="footerwrapper__right-Footer">
-        <h1> Subscribe to Our Luxury Living Newsletter</h1>
-        <form>
-            <input type="text" class="footerwrapper__text-input" name="userName" placeholder="Your name">
-            <input type="email" class="footerwrapper__text-input" placeholder="Your email">
-            <input type="submit" class="footerwrapper__subscribe-btn" value="Subscribe">
-        </form>
-    </div>
-
-</div>
-<div class="footerwrapper-bottom">
-    <div class="icon-row">
-        <a href="https://facebook.com/" target="_blank">
-            <img src="{{ URL::asset('assets/svg/facebook.svg') }}" alt="facebook">
-        </a>
-        <a href="https://instagram.com/" target="_blank">
-            <img src="{{ URL::asset('assets/svg/instagram.svg') }}" alt="instagram">
-        </a>
-        <a href="https://x.com/" target="_blank"><img src="{{ URL::asset('assets/svg/twitter.svg') }}" alt="x"></a>
+    <div class="footerwrapper-bottom">
+        <div class="icon-row">
+            <a href="https://facebook.com/" target="_blank">
+                <img src="{{ URL::asset('assets/svg/facebook.svg') }}" alt="facebook">
+            </a>
+            <a href="https://instagram.com/" target="_blank">
+                <img src="{{ URL::asset('assets/svg/instagram.svg') }}" alt="instagram">
+            </a>
+            <a href="https://x.com/" target="_blank"><img src="{{ URL::asset('assets/svg/twitter.svg') }}"
+                    alt="x"></a>
+        </div>
     </div>
 </div>
