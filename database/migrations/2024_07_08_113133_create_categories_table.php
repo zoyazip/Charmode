27d47973e5afd41c4e87627b45db3ad06f2c88c4
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-07-09 11:06:39 +0300

    resolve merge conflict

--- database/migrations/2024_07_08_113133_create_categories_table.php
+++ database/migrations/2024_07_08_113133_create_categories_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('category');
+            $table->string('sub-category');
             $table->timestamps();
         });
     }
