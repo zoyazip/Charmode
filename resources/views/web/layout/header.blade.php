2365d61068dd58a3e3da1b556d0b32de2095eb7a
nikita <nikita@plotnikov.lv>
2024-07-10 08:59:45 +0300

    adaptive navbar

--- resources/views/web/layout/header.blade.php
+++ resources/views/web/layout/header.blade.php
@@ -1,32 +1,53 @@
-<div class="container">
-    <div class="inner-container">
-        <div class="navbar">
-            <div class="navbar__top">
-                <img class="navbar__logo" src="assets/svg/logo.svg"></img>
-                <div class="navbar__search-container">
-                    <input type="text" placeholder="Find your most cheapest luxury brand...">
-                    <img class="navbar__search-icon" src="assets/svg/search-icon.svg"></img>
-                </div>
-                <div class="navbar__icons">
-                    <img src="assets/svg/shopping-basket.svg" alt="Cart">
-                    <img src="assets/svg/user.svg" alt="User">
-                </div>
-            </div>
-            <div class="navbar__links">
-                <div class="navbar__link-container navbar--active"><a href="#home">Homezzz</a></div>
-                <div class="navbar__link-container"><a href="#about">About</a></div>
-                <div class="navbar__link-container"><a href="#services">Services</a></div>
-                <div class="navbar__link-container"><a href="#products">Products</a></div>
-                <div class="navbar__link-container"><a href="#contact">Contact</a></div>
-                {{-- @foreach($categories as $category)
-                    <div class="navbar__link-container"><a href="#{{ strtolower($category->category) }}">{{ $category->category }}</a></div>
-                @endforeach --}}
-            </div>
-        </div>
-    </div>
+<div class="inner-container">
+	<div class="navbar">
+		<div class="navbar__top">
+			<img class="navbar__menu" src="assets/svg/burger-menu-icon.svg" alt="Logo" />
+			<img class="navbar__logo" src="assets/svg/logo.svg" alt="Logo" />
+			<div class="navbar__search-container">
+				<input
+					class="navbar__search-text"
+					type="text"
+					placeholder="Find your most cheapest luxury brand..."
+				/>
+				<img
+					class="navbar__search-icon"
+					src="assets/svg/search-icon.svg"
+					alt="Search"
+				/>
+			</div>
+			<div class="navbar__icons">
+				<img src="assets/svg/shopping-basket.svg" alt="Cart" />
+				<img src="assets/svg/user.svg" alt="User" />
+			</div>
+		</div>
+		<div class="navbar__links">
+			<div class="navbar__link-container navbar--active">
+				<a href="#home">Living room</a>
+			</div>
+			<div class="navbar__link-container">
+				<a href="#about">Bathroom</a>
+			</div>
+			<div class="navbar__link-container">
+				<a href="#services">Kitchen</a>
+			</div>
+			<div class="navbar__link-container">
+				<a href="#products">Bedroom</a>
+			</div>
+			<div class="navbar__link-container">
+				<a href="#contact">Foyer</a>
+			</div>
+			{{-- <!-- Commented out dynamic category links -->
+			<!-- @foreach($categories as $category)
+				<div class="navbar__link-container"><a href="#{{ strtolower($category->category) }}">{{ $category->category }}</a></div>
+			@endforeach --> --}}
+		</div>
+		<div class="navbar__bottom-search">
+			<input
+				class="navbar__search-text"
+				type="text"
+				placeholder="Find your most cheapest luxury brand..."
+			/>
+			<img class="navbar__search-icon" src="assets/svg/search-icon.svg" alt="Search" />
+		</div>
+	</div>
 </div>
-
-{{-- @push('scripts')
-    <script  src="{{ URL::asset('js/header/header_ajax.js') }}"></script>
-@endpush --}}
-{{-- todo: connect this script --}}
