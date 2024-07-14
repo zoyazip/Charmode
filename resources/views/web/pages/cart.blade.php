1909866b1ecbcbf6b6668c5d57c5a91d995e04cd
Denis Chernov <chernoff.cs@gmail.com>
2024-07-14 17:12:25 +0300

    Fixed cart layout

--- resources/views/web/pages/cart.blade.php
+++ resources/views/web/pages/cart.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'Cart page')
 
 @push('styles')
-
     {{-- item card styler --}}
     <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/cart-item-card.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/sum-up.css') }}" />
-{{--    <script src="{{ URL::asset('js/cart/item-card.js') }}"></script>--}}
+    {{--    <script src="{{ URL::asset('js/cart/item-card.js') }}"></script> --}}
 @endpush
 
 @section('content')
-    <div class="main-container">
+    {{-- removed main container class --}}
+    <div class="">
         <div class="inner-container">
             @include('components/cart-item-card')
             @include('components/sum-up')
