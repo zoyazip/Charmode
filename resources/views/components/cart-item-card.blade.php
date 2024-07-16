9b96a65be6a52119263cd4de2494530d9863e4b3
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-07-16 21:20:40 +0300

    updated

--- resources/views/components/cart-item-card.blade.php
+++ resources/views/components/cart-item-card.blade.php
@@ -1,4 +1,4 @@
-<div class="item-wrapper middle-wrapper id-1">
+<div class="item-wrapper middle-wrapper id-{{$product->id}}">
 
     <div class="item-wrapper__left">
         <img class="cart-image" src="{{ URL::asset('assets/chair-placeholder.png')}}" alt="chair">
@@ -14,11 +14,11 @@
             <p class="item-wrapper__free-delivery">{{$product->shippingCost}}</p>
         </div>
         <div class="item-wrapper__right-right">
-            <p class="item-wrapper__end-price">366€</p>
+            <p class="item-wrapper__end-price">{{$product->quantity*$product->newPrice}}€</p>
             <form>
                 <p class="item-wrapper__more-or-less">
                     <a class="item-wrapper__inc-btn" ><span class="item-wrapper__more-or-less-item minus" >-</span></a>
-                    <input type="number" min="1" max="10" value="1" class="item-wrapper__more-or-less-item item-wrapper__price">
+                    <input type="number" min="1" max="{{$product->stockQuantity}}" value="{{$product->quantity}}" class="item-wrapper__more-or-less-item item-wrapper__price">
                     {{--                inputam max jāsaņem no datubazes, tā pat arī ar value--}}
                     <a class="item-wrapper__inc-btn" ><span class="item-wrapper__more-or-less-item plus" >+</span></a>
                 </p>
