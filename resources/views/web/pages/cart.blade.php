1c17593e10ebeb326624cc3d864a8f1af3103a65
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-07-16 12:55:32 +0300

    created checkout component, added to cart page checkout section

--- resources/views/web/pages/cart.blade.php
+++ resources/views/web/pages/cart.blade.php
@@ -6,6 +6,9 @@
     {{-- item card styler --}}
     <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/cart-item-card.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/sum-up.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/checkout-button.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/pages/cart/checkout-section.css') }}" />
+
     {{--    <script src="{{ URL::asset('js/cart/item-card.js') }}"></script> --}}
 @endpush
 
@@ -15,6 +18,19 @@
         <div class="inner-container">
             @include('components/cart-item-card')
             @include('components/sum-up')
+{{--            @include('components/checkout-button')--}}
+
+            <div class="middle-wrapper checkout-section">
+                <div class="checkout-section__left">
+                    <button class="checkout-section__continue-btn">Continue shopping</button>
+                    <button>Reset</button>
+                </div>
+                <div class="checkout-section__right">
+
+                <x-checkout-button checkoutPrice="30"></x-checkout-button>
+                </div>
+
+            </div>
         </div>
     </div>
 @endsection
