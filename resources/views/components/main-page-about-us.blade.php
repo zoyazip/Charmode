150cbdd5a092af36157c24e33a75b020da4d96b7
Denis Chernov <chernoff.cs@gmail.com>
2024-07-17 17:55:53 +0300

    Designed admin page and admin product page

--- resources/views/components/main-page-about-us.blade.php
+++ resources/views/components/main-page-about-us.blade.php
@@ -1,11 +1,11 @@
 <div class="about-us w-full h-[60vh] border border-main-green rounded-2xl flex flex-col justify-between text-white p-10">
     <div class="us flex justify-between items-start">
-        <img class="w-[32px] md:w-[48px]" src="assets/svg/star.svg" />
+        <img class="w-[32px] md:w-[48px]" src="assets/svg/star.svg" alt="star" />
         <h3 class="text-main-green font-bold text-6xl lg:text-8xl">WE</h3>
     </div>
     <div class="abt-text"><p class="text-main-green text-xl md:text-2xl lg:text-3xl text-justify indent-48">We're more than just an e-commerce platform; we're your go-to destination for transforming houses into personalized homes. At our online furniture marketplace, we take pride in curating a diverse and elegant collection of furniture pieces. </p></div>
     <div class="you flex justify-between items-end">
         <h3 class="text-main-green font-bold text-6xl lg:text-8xl">US</h3>
-        <img class="w-[32px] md:w-[48px]" src="assets/svg/star.svg" />
+        <img class="w-[32px] md:w-[48px]" src="assets/svg/star.svg" alt="star" />
     </div>
 </div>
--- resources/views/web/pages/admin.blade.php
+++ resources/views/web/pages/admin.blade.php
@@ -7,27 +7,30 @@
 @endpush
 
 @section('content')
-    <div class="admin__container">
-        <div class="hi__div">
-            <h1>Hi, Admin!</h1>
-            <button onclick="window.location='/adminproducts/create'" class="admin__btn green">Add product</button>
-            <button onclick="window.location='/orders'" class="admin__btn green">Orders</button>
+    <div class="admin__container pb-10">
+        <div class="hi__div py-10">
+            <h1 class="text-main-green">Hi, Admin!</h1>
+            <div class="">
+                <button onclick="window.location='/orders'" class="admin__btn">Orders</button>
+                <button onclick="window.location='/adminproducts/create'" class="px-4 py-2 rounded-lg bg-main-green text-white hover:shadow-lg transition-all">Add product</button>
+            </div>
+            
         </div>
         <div class="one-product__row">
-            <div class="one-product__row-left">
-                <div>Product Code</div>
-                <div>Product Name</div>
+            <div class="one-product__row-left font-bold text-main-green">
+                <div><span>Product Code</span></div>
+                <div><span>Product Name</span></div>
             </div>
-            <div>Show More</div>
+            <div class="font-bold text-main-green">Action</div>
         </div>
             @if (isset($products))
                 @foreach ($products as $product)
-                <div class="one-product__row">
+                <div class="one-product__row hover:bg-green-100 transition-all">
                     <div class="one-product__row-left">
                         <div>{{$product->product_code}}</div>
                         <div>{{$product->name}}</div>
                     </div>
-                    <button onclick="window.location='/adminproducts/{{$product->id}}'">Show More</button>
+                    <button onclick="window.location='/adminproducts/{{$product->id}}'"><img src="{{ URL::asset('assets/svg/link_arrow.svg') }}" /></button>
                 </div>
                 @endforeach
             @endif
