480cb626914162573171e45ac47b871ffac5dbc2
nikita <nikita@plotnikov.lv>
2024-07-18 15:25:23 +0300

    fix checkout functionality

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\CartItem;
+use App\Models\Product;
 
 use Illuminate\Support\Facades\DB;
 
@@ -16,13 +17,13 @@ use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    
+
     public function openCheckoutPage() {
         return view('web.pages.checkout');
     }
 
     public function checkInput(Request $request): RedirectResponse {
-        
+
         // dd($request);
         $checkoutemail = "";
         $city = "";
@@ -49,9 +50,11 @@ class CheckoutController extends Controller
             //     $address = "Address";
             // }
         // }
-        
+
 
         // dd($request);
+
+        $products = Product::all();
 
         $user_id = NULL;
         $addedItems = NULL;
@@ -63,8 +66,9 @@ class CheckoutController extends Controller
             $addedItems = DB::table('cart_items')->where('user_id', '=', Auth::id())->get();
             if($addedItems !== NULL){
                 foreach($addedItems as $item) {
-                    $totalCost = $totalCost + $item->product->newPrice;
-                    $deliveryCost = $deliveryCost + $item->product->shippingCost;
+                    $matchingProduct = $products->firstWhere('id', $item->product_id);
+                    $totalCost = $totalCost + $matchingProduct->newPrice;
+                    $deliveryCost = $deliveryCost + $matchingProduct->shippingCost;
                 }
             }
         } else {
